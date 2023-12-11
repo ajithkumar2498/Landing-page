6ad8e3ee8553873629695c918d98aeac63e61027
ajithkumar2498 <ajibhc24@gmail.com>
2023-12-11 16:09:40 +0530

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -74,10 +74,8 @@ let testimonials = [{
                 <div className="row justify-content-center">
                     <div className="col-xl-6">
                         <div className="text-center text-white">
-                            {/* <!-- Page heading--> */}
                             <h1 className="mb-5">Generate more leads with a professional landing page!</h1>
                                   <form className="form-subscribe" id="contactForm" data-sb-form-api-token="API_TOKEN">
-                                {/* <!-- Email address input--> */}
                                 <div className="row">
                                     <div className="col">
                                         <input className="form-control form-control-lg" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required,email" />
@@ -86,10 +84,6 @@ let testimonials = [{
                                     </div>
                                     <div className="col-auto"><button className="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
                                 </div>
-                                {/* <!-- Submit success message-->
-                                <!---->
-                                <!-- This is what your users will see when the form-->
-                                <!-- has successfully submitted--> */}
                                 <div className="d-none" id="submitSuccessMessage">
                                     <div className="text-center mb-3">
                                         <div className="fw-bolder">Form submission successful!</div>
@@ -97,10 +91,6 @@ let testimonials = [{
                                         <a className="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
                                     </div>
                                 </div>
-                                {/* <!-- Submit error message-->
-                                <!---->
-                                <!-- This is what your users will see when there is-->
-                                <!-- an error submitting the form--> */}
                                 <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
                             </form>
                         </div>
